be44b4330bc53de822752803e0fd036d26006c21
huyanzhenb <2724584248@qq.com>
2025-02-28 01:14:55 +0800

    接口测试通过

--- app/[locale]/weather/page.tsx
+++ app/[locale]/weather/page.tsx
@@ -1,3 +1,4 @@
+import { getWeatherData } from '@/api/localWeather';
 import { getTranslations } from 'next-intl/server';
 
 interface PageProps {
@@ -11,38 +12,40 @@ export default async function WeatherPage({
 }: PageProps) {
     const { locale } = await params; 
   const t = await getTranslations('Weather');
+  const location = "麻城"; // 北京的 Location ID
+  const weatherData = await getWeatherData(location);
+  console.log(weatherData)
 
   return (
-    <div className="p-4">
-      <div className="grid grid-cols-2 gap-4">
+    <div className="p-4 bg-primary h-full">
+      <div className="grid h-full grid-cols-2 gap-4">
         {/* 顶部横跨两列的大块 */}
         <div className="col-span-2 bg-blue-200 rounded-lg p-4 shadow-md">
-          <h2 className="text-lg font-semibold">{t('title')}</h2>
-          <p>{t('details')}</p>
-          <h1 className='text-[50px]'>{locale}</h1>
+          <h2 className="text-lg text-[#4791ff] font-semibold">{t('title')}</h2>
+          <p className="text-[#4791ff]">{t('details')}</p>
         </div>
 
         {/* 左侧块 */}
         <div className="bg-green-200 rounded-lg p-4 shadow-md">
-          <h2 className="text-lg font-semibold">{t('today')}</h2>
-          <p>{t('current')}</p>
+          <h2 className="text-lg text-[#4791ff] font-semibold">{t('today')}</h2>
+          <p className="text-[#4791ff]">{t('current')}</p>
         </div>
 
         {/* 右侧块 */}
         <div className="bg-yellow-200 rounded-lg p-4 shadow-md">
-          <h2 className="text-lg font-semibold">{t('forecast')}</h2>
-          <p>{t('future')}</p>
+          <h2 className="text-lg text-[#4791ff] font-semibold">{t('forecast')}</h2>
+          <p className="text-[#4791ff]">{t('future')}</p>
         </div>
 
         {/* 底部两个横跨两列的块 */}
         <div className="col-span-2 bg-purple-200 rounded-lg p-4 shadow-md">
-          <h2 className="text-lg font-semibold">{t('warning')}</h2>
-          <p>{t('warningInfo')}</p>
+          <h2 className="text-lg text-[#4791ff] font-semibold">{t('warning')}</h2>
+          <p className="text-[#4791ff]">{t('warningInfo')}</p>
         </div>
 
         <div className="col-span-2 bg-pink-200 rounded-lg p-4 shadow-md">
-          <h2 className="text-lg font-semibold">{t('lifeIndex')}</h2>
-          <p>{t('lifeAdvice')}</p>
+          <h2 className="text-lg text-[#4791ff] font-semibold">{t('lifeIndex')}</h2>
+          <p className="text-[#4791ff]">{t('lifeAdvice')}</p>
         </div>
       </div>
     </div>
